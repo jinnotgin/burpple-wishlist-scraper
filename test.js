d90d96ffde1f52020ab69e9866e7a364d8027a58
Jin <me@thelinjin.com>
2021-06-07 16:45:32 +0800

    Test implementation of venue scraping

--- test.js
+++ test.js
@@ -1,33 +1,43 @@
+import logger from "./logger.js";
 import { scrapeVenue } from "./utils/scraperBurpple.js";
 import JsonFileStream from "./utils/jsonFileStream.js";
 import * as constants from "./constants.js";
-import os from "os";
+
 const { WISHLIST_DATAFILE, VENUES_DATAFILE } = constants;
 const wishlist_fileStream = new JsonFileStream(WISHLIST_DATAFILE);
-const venue_fileStream = new JsonFileStream(VENUES_DATAFILE, "object");
+const venue_fileStream = new JsonFileStream(VENUES_DATAFILE, "dictionary");
 
-venue_fileStream.init();
-
+let count = 0;
 // TODO: the following should be moved to JsonFileStream
-const readline = wishlist_fileStream.readline;
-readline.on("line", async (line) => {
-	const isValidData = line.match(/{.*},/) !== null;
-	if (!!!isValidData) return false;
+const burppleWishlistVenueTask = async () => {
+	try {
+		venue_fileStream.init();
 
-	const data = JSON.parse(line.replace("},", "}"));
-	console.log(data);
+		const readline = wishlist_fileStream.readline;
 
-	const { url } = data;
-	const venueData = await scrapeVenue(url);
-	console.log(venueData);
+		let linesToProcess = 0;
+		readline.on("line", async (line) => {
+			const isValidData = line.match(/{.*}/) !== null;
+			if (!!!isValidData) return false;
 
-	venue_fileStream.append([{ id: venueData.id, data: venueData }]);
+			const data = JSON.parse(line.replace(",{", "{")); // remove the first comma in the line, if any
+			const { url } = data;
+			linesToProcess++;
+			const venueData = await scrapeVenue(url);
+			linesToProcess--;
+			venueData["inWishlist"] = true;
+			console.log(venueData);
 
-	// TODO get last line detection
-	if (line == "" || line == os.EOL) {
-		console.log("eof, last line is", lastLine);
-
-		venue_fileStream.end();
-		return;
+			// venue_fileStream.append([{ id: venueData.id, data: venueData }]);
+			venue_fileStream.append([venueData]);
+			if (linesToProcess === 0) venue_fileStream.end();
+		});
+	} catch (e) {
+		logger.error(e);
+		logger.error(
+			"During page scraping, encountered an exception. Routine will now terminate."
+		);
 	}
-});
+};
+
+burppleWishlistVenueTask();
