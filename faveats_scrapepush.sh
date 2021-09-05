abf147c63acc00465b89614acb284dbfd4b8420c
Jin <me@thelinjin.com>
2021-09-06 00:58:01 +0800

    Create faveats_scrapepush.sh

---
+++ faveats_scrapepush.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT=`which git`
+NODE=`which node`
+REPO_DIR=/home/jin/burpple-wishlist-scraper/
+
+cd ${REPO_DIR}
+
+${GIT} pull
+${NODE} index.js 
+
+${GIT} add --all .
+CURRENTDATE=`date +"%Y-%m-%d %T"`
+${GIT} commit -m "Updated data on $CURRENTDATE"
+${GIT} push
