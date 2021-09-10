d40338e4df805332ce8abd2938ae898047b201d8
Jin (OVH <me@thelinjin.com>
2021-09-11 01:29:03 +0800

    Updated data on 2021-09-11 01:29:03

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import burppleWishlistTask from "./burppleWishlistTask.js";
 // import burppleWishlistVenueTask from "./burppleWishlistVenueTask.js";
 import burppleVenueTask from "./burppleVenueTask.js"
 import generateUsersJson from "./generateUsersJson.js"
-import firestore from "./utils/firebase.js";
+// import firestore from "./utils/firebase.js";
 
 // TODO: update this to a method that won't result in large memory usage
-const usernamesArray = await firestore.getUsernames();
-// const usernamesArray = ["RunningMan", "sarahs"];
+//const usernamesArray = await firestore.getUsernames();
+const usernamesArray = ["RunningMan", "sarahs"];
 
 
 burppleWishlistTask(usernamesArray, async () => {
